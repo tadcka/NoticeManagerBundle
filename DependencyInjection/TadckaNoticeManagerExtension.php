904f34aeb391d2b30cbd815c3cddd460cbc48b3f
Tadas <tadcka89@gmail.com>
2013-11-19 23:15:01 +0200

    Update TadckaNoticeManagerExtension.php

--- DependencyInjection/TadckaNoticeManagerExtension.php
+++ DependencyInjection/TadckaNoticeManagerExtension.php
@@ -42,7 +42,7 @@ class TadckaNoticeManagerExtension extends Extension
     public function load(array $configs, ContainerBuilder $container)
     {
         $configuration = new Configuration();
-        $config = $this->processConfiguration($configuration, $configs);
+        $this->processConfiguration($configuration, $configs);
 
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.xml');
